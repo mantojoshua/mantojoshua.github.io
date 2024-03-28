b31cfa38ee7333a2ba2e5945a62d5f8692894f9b
mantojoshua <92662356+mantojoshua@users.noreply.github.com>
2024-03-28 15:49:11 +0800

    added back to top button

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const element = document.getElementById('myName');
-    typeEffect("Hello, I'm Joshua Manto", element);
-  });
-  
-  function typeEffect(text, element) {
-    let i = 0;
-    const interval = setInterval(() => {
-      if (i < text.length) {
-        element.textContent += text.charAt(i);
-        i++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 100);
+  const element = document.getElementById('myName');
+  typeEffect("Hello, I'm Joshua Manto", element);
+
+  const backToTopButton = document.getElementById('backtotop');
+  if (backToTopButton) {
+    backToTopButton.addEventListener('click', () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
   }
+});
+
+function typeEffect(text, element) {
+  let i = 0;
+  const interval = setInterval(() => {
+    if (i < text.length) {
+      element.textContent += text.charAt(i);
+      i++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 100);
+}
