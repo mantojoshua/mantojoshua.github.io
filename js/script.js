9f4399770976e53608632832a2bda461c8b8e33f
mantojoshua <92662356+mantojoshua@users.noreply.github.com>
2024-08-25 17:12:13 +0800

    minor changes

--- js/script.js
+++ js/script.js
@@ -32,3 +32,11 @@ function typeEffect(text, element) {
     }
   }, 100);
 }
+
+window.onscroll = function() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("loadingBar").style.width = scrolled + "%";
+  console.log(scrolled);
+};
