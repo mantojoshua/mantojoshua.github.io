993c51e8c830a67bfb819026a84db310391a677a
mantojoshua <92662356+mantojoshua@users.noreply.github.com>
2026-06-27 20:54:40 +0800

    minor spacing edit

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,12 +2,12 @@ import { test, expect } from '@playwright/test';
 
 test('Verify that all the navigation links are working', async ({ page }) => {
 
-await page.goto('https://mantojoshua.github.io/');
-await page.getByRole('link', { name: '🏠Home' }).click();
-await page.getByRole('link', { name: '💻Work' }).click();
-await page.getByRole('link', { name: '🏆Certificates' }).click();
-await page.getByRole('link', { name: '🔧Skills' }).click();
-await page.getByRole('button', { name: 'Let\'s Connect' }).click();
-await page.getByRole('link', { name: '🏠Home' }).click();
+    await page.goto('https://mantojoshua.github.io/');
+    await page.getByRole('link', { name: '🏠Home' }).click();
+    await page.getByRole('link', { name: '💻Work' }).click();
+    await page.getByRole('link', { name: '🏆Certificates' }).click();
+    await page.getByRole('link', { name: '🔧Skills' }).click();
+    await page.getByRole('button', { name: 'Let\'s Connect' }).click();
+    await page.getByRole('link', { name: '🏠Home' }).click();
 
 });
