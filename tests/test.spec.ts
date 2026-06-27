3fb97f2feb99feac4a9569bcd7eb9bc69725b849
mantojoshua <92662356+mantojoshua@users.noreply.github.com>
2026-06-27 19:51:01 +0800

    created a failing test case to see github actions deployment

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -8,6 +8,8 @@ await page.getByRole('link', { name: '💻Work' }).click();
 await page.getByRole('link', { name: '🏆Certificates' }).click();
 await page.getByRole('link', { name: '🔧Skills' }).click();
 await page.getByRole('button', { name: 'Let\'s Connect' }).click();
-await page.getByRole('link', { name: '🏠Homz' }).click();
+await page.getByRole('link', { name: '🏠Home' }).click();
+
+test.fail();
 
 });
