15e4c2ad2bf7619ab7e8fe7bec5635e87149824e
mantojoshua <92662356+mantojoshua@users.noreply.github.com>
2026-06-28 13:56:45 +0800

    added more test cases

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,13 +1,55 @@
 import { test, expect } from '@playwright/test';
 
+let baseUrl:string = 'https://mantojoshua.github.io/'
+
 test('Verify that all the navigation links are working', async ({ page }) => {
 
-    await page.goto('https://mantojoshua.github.io/');
+    await page.goto(baseUrl);
     await page.getByRole('link', { name: '🏠Home' }).click();
     await page.getByRole('link', { name: '💻Work' }).click();
     await page.getByRole('link', { name: '🏆Certificates' }).click();
     await page.getByRole('link', { name: '🔧Skills' }).click();
     await page.getByRole('button', { name: 'Let\'s Connect' }).click();
     await page.getByRole('link', { name: '🏠Home' }).click();
+
+});
+
+test('Verify that the Work Experience is visible', async ({ page }) => {
+
+    await page.goto(baseUrl)
+    await page.getByRole('link', { name: '💻Work' }).click()
+    await expect(page.getByText("Work Experience"))
+
+});
+
+test('Verify that the Certificates are visible', async ({ page }) => {
+
+    await page.goto(baseUrl)
+    await page.getByRole('link', { name: '🏆Certificates' }).click()
+    await expect(page.locator('div').filter({ hasText: '🏆Certificates' }))
+
+});
+
+test('Verify that the Skills are visible', async ({ page }) => {
+
+    await page.goto(baseUrl)
+    await page.getByRole('link', { name: '🔧Skills' }).click();
+    await expect(page.locator('div').filter({ hasText: '🔧Skills' }))
+
+});
+
+test('Verify that the Contacts are visible', async ({ page }) => {
+
+    await page.goto(baseUrl)
+    await page.getByRole('button', { name: 'Let\'s Connect' }).click();
+    await expect(page.getByRole('heading', { name: '📞Contact Me' }))
+
+});
+
+test('Verify that the minigame are visible', async ({ page }) => {
+
+    await page.goto(baseUrl)
+    await page.getByRole('link', { name: '🏠Home' }).click();
+    await expect(page.locator('embed').contentFrame().locator('#gameVideo'))
 
 });
