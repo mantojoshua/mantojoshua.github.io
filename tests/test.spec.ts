921c8b68f6c65a4982211e74302c713ec1577f03
mantojoshua <92662356+mantojoshua@users.noreply.github.com>
2026-07-04 14:23:29 +0800

    minor improvements

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach(async ({page}) => {
+    await page.goto('/');
+})
+
 test('Verify that all the navigation links are working', async ({ page }) => {
 
-    await page.goto('/');
     await page.getByRole('link', { name: '🏠Home' }).click();
     await expect(page.locator('#home')).toBeInViewport();
     await page.getByRole('link', { name: '💻Work' }).click();
@@ -19,40 +22,35 @@ test('Verify that all the navigation links are working', async ({ page }) => {
 
 test('Verify that the Work Experience is visible', async ({ page }) => {
 
-    await page.goto('/');
     await page.getByRole('link', { name: '💻Work' }).click()
-    await expect(page.getByText("Work Experience"))
+    await expect(page.getByText("Work Experience")).toBeVisible();
 
 });
 
 test('Verify that the Certificates are visible', async ({ page }) => {
 
-    await page.goto('/');
     await page.getByRole('link', { name: '🏆Certificates' }).click()
-    await expect(page.locator('div').filter({ hasText: '🏆Certificates' }))
+    await expect(page.locator('div').filter({ hasText: '🏆Certificates' })).toBeVisible();
 
 });
 
 test('Verify that the Skills are visible', async ({ page }) => {
 
-    await page.goto('/');
     await page.getByRole('link', { name: '🔧Skills' }).click();
-    await expect(page.locator('div').filter({ hasText: '🔧Skills' }))
+    await expect(page.locator('div').filter({ hasText: '🔧Skills' })).toBeVisible();
 
 });
 
 test('Verify that the Contacts are visible', async ({ page }) => {
 
-    await page.goto('/');
     await page.getByRole('button', { name: 'Let\'s Connect' }).click();
-    await expect(page.getByRole('heading', { name: '📞Contact Me' }))
+    await expect(page.getByRole('heading', { name: '📞Contact Me' })).toBeVisible();
 
 });
 
 test('Verify that the minigame are visible', async ({ page }) => {
 
-    await page.goto('/');
     await page.getByRole('link', { name: '🏠Home' }).click();
-    await expect(page.locator('embed').contentFrame().locator('#gameVideo'))
+    await expect(page.locator('embed').contentFrame().locator('#gameVideo')).toBeVisible();
 
 });
